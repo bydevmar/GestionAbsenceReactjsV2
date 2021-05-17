073b564de3bebbcb445b73ee89cfadac06285dbc
DevLop99 <83462650+DevLop99@users.noreply.github.com>
2021-05-17 17:43:17 +0000

    ADD DEMO DASHBOARD

--- src/Routes/main.routes.js
+++ src/Routes/main.routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route  , Switch} from 'react-router-dom'
 import Login from "../Components/login"
 import Home from "../Components/Home"
+import Dashboard from '../Components/Dashboard/Dashboard';
 
 
 
@@ -11,6 +12,7 @@ import Home from "../Components/Home"
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/login" exact component={Login} />
+                <Route path="/dashboard" exact component={Dashboard} />
                 <Route path="*" exact component={Home} />
             </Switch>
         </div>
